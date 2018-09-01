95e9d9636704a1879095e70a66b3cb0f8bd86f8c
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-08-31 23:27:47 -0400

    setup Phaser and add resize event listner

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+import Phaser from "phaser";
+
 console.log("hello");
+
+let game;
+
+window.onload = () => {
+  let gameConfig = {
+    type: Phaser.CANVAS,
+    width: 550,
+    height: 550,
+    backgroundColor: 0x880044
+  };
+
+  game = new Phaser.Game(gameConfig);
+
+  window.focus();
+  resize();
+  window.addEventListener("resize", resize, false);
+};
+
+function resize() {
+  let canvas = document.querySelector("canvas");
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const windowRatio = windowWidth / windowHeight;
+  const gameRatio = game.config.width / game.config.height;
+
+  if (windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = windowWidth / gameRatio + "px";
+  } else {
+    canvas.style.width = windowHeight * gameRatio + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
